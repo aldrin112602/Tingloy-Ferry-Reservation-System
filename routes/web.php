ab0b0cc049e9557a3cb99e6124dab0f93924ebdf
Aldrin Caballero <caballeroaldrin02@gmail.com>
2025-05-12 19:02:21 +0800

    Added middleware for user roles

--- routes/web.php
+++ routes/web.php
@@ -7,32 +7,27 @@ use App\Http\Controllers\RouteController;
 use App\Http\Controllers\StaffController;
 use App\Models\Route as RouteSchedule;
 
+
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
-
-
-
     // STAFF ROUTES
-    Route::prefix('staff')->group(function () {
+    Route::prefix('staff')->middleware('role:staff')->group(function () {
         Route::get('passengers', [StaffController::class, 'passengerList'])->name('staff.passengers');
         Route::get('scan_qr', [StaffController::class, 'scanQrCode'])->name('staff.scan_qr');
     });
 
-
-
-
     // PASSENGER ROUTES
-    Route::prefix('passenger')->group(function () {
+    Route::prefix('passenger')->middleware('role:passenger')->group(function () {
         Route::get('book_ticket', function () {
             $routes = RouteSchedule::latest()->get();
-            
             return Inertia::render('features/passenger/BookTicket', ['routes' => $routes]);
         })->name('passenger.book_ticket');
 
@@ -40,15 +35,15 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::post('book', [BookingController::class, 'store'])->name('bookings.store');
     });
 
-
     // ADMIN ROUTES
-    Route::prefix('admin')->group(function () {
+    Route::prefix('admin')->middleware('role:admin')->group(function () {
         Route::get('schedule', [RouteController::class, 'index'])->name('admin.schedule.index');
         Route::post('schedule', [RouteController::class, 'store'])->name('admin.schedule.store');
         Route::put('schedule', [RouteController::class, 'put'])->name('admin.schedule.put');
         Route::delete('schedule/{id}', [RouteController::class, 'delete'])->name('admin.schedule.delete');
     });
 });
+
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
